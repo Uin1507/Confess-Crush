37a57a99097bb4c51aea6bc8018ec233a8dbb798
Uin1507 <100406967+Uin1507@users.noreply.github.com>
2022-02-25 15:10:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,5 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Chưa Đọc nên chưa biết :)))',
     messDesc: 'Hehe.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+
+   
